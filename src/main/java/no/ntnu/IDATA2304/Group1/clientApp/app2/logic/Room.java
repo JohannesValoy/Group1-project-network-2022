fed32e3dcc394e90fef02af8f36405edc77e7347
eirilovi <eirilovi@stud.ntnu.no>
2022-10-14 18:03:29 +0200

    Started on Room and Sensor class

--- src/main/java/no/ntnu/IDATA2304/Group1/clientApp/app2/logic/Room.java
+++ src/main/java/no/ntnu/IDATA2304/Group1/clientApp/app2/logic/Room.java
@@ -1,9 +1,33 @@
 package no.ntnu.IDATA2304.Group1.clientApp.app2.logic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * represents a room in a house with an arraylist with one or more temperature sensors.
  */
 public class Room {
 
+    public static ArrayList<Sensor> sensorList = new ArrayList<>();
+
+    public void addSensor (Sensor s) {
+        sensorList.add(s);
+    }
+
+    public void removeSensor (Sensor s) {
+        sensorList.add(s);
+    }
+
+    public List getListOfSensors() {
+        return sensorList;
+    }
+
+    public void printListByType(int type) {
+            for (Sensor search : sensorList) {
+                if(search.getName().contains(type)) {
+                    System.out.println(search.toString());
+                }
+            }
+    }
 
 }
--- src/main/java/no/ntnu/IDATA2304/Group1/clientApp/app2/logic/Sensor.java
+++ src/main/java/no/ntnu/IDATA2304/Group1/clientApp/app2/logic/Sensor.java
@@ -4,5 +4,16 @@ package no.ntnu.IDATA2304.Group1.clientApp.app2.logic;
  * Receives data from sensor using https request.
  */
 public class Sensor {
+    private int humidity;
+    private int temperature;
 
+    public getName(int type) {
+        if(type == 1) {
+            System.out.println(humidity);
+        }
+        if (type == 2) {
+            System.out.println(temperature);
+        }
+
+    }
 }
