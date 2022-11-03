f37beb0d487ca29948867379bb659f25d62d431c
JeyKey09 <62012201+JeyKey09@users.noreply.github.com>
2022-11-03 13:02:27 +0100

    Changed Sensor to use room Records

--- src/main/java/no/ntnu/IDATA2304/Group1/data/Sensor.java
+++ src/main/java/no/ntnu/IDATA2304/Group1/data/Sensor.java
@@ -1,6 +1,7 @@
 package no.ntnu.idata2304.group1.data;
 
 
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
@@ -9,55 +10,68 @@ import java.util.Random;
  * Represents a sensor doing measurement. Sensor type is decided by string type.
  */
 public class Sensor {
-    private List<Integer> historyLog; //Log of all readings from the sensor
+    public enum Types {
+        TEMPERATURE("Temperature"), HUMIDITY("Humidity");
+
+        private String name;
+
+        Types(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    private List<RoomRecord> historyLog; // Log of all readings from the sensor
     private Integer currentLevel = 0; // Current reading
     private Random randomGen; // Random generator making example data
-    private String type; // Type of sensor
+    private final Types type; // Type of sensor
     private int id; // ID of sensor
 
-    public Sensor(String type, int id) {
+    public Sensor(Types type, int id) {
         this.historyLog = new LinkedList<>();
         this.randomGen = new Random();
         this.type = type;
         this.id = id;
-        //Loads ExampleValues
-        for(int i = 0; i < 10; i++){
+        // Loads ExampleValues
+        for (int i = 0; i < 10; i++) {
             updateReading();
         }
     }
 
     /**
      * Returns the name of the sensor.
+     * 
      * @return string Name.
      */
-    public String getName(){
-        return this.type;
-    }
-
-    public void setName(String name){
-        this.type = name;
+    public String getTypeName() {
+        return this.type.getName();
     }
 
     /**
      * Updates sensor readings
      */
     public void updateReading() {
-        this.currentLevel = (this.currentLevel + (500 - randomGen.nextInt(1000))/200);
-        historyLog.add(this.currentLevel);
+        this.currentLevel = (this.currentLevel + (500 - randomGen.nextInt(1000)) / 200);
+        historyLog.add(new RoomRecord(new Date(), this.currentLevel));
         System.out.println(historyLog.size());
     }
 
     /**
      * Returns the history log of the sensor.
+     * 
      * @return List historyLog.
      */
-    public List<Integer> getHistoryLog() {
+    public List<RoomRecord> getHistoryLog() {
         updateReading();
         return this.historyLog;
     }
 
     /**
      * Returns the ID of the sensor.
+     * 
      * @return int id.
      */
     public int getId() {
@@ -66,9 +80,10 @@ public class Sensor {
 
     /**
      * Returns the type of the sensor.
+     * 
      * @return String type.
      */
-    public String getType() {
+    public Types getType() {
         return this.type;
     }
 
