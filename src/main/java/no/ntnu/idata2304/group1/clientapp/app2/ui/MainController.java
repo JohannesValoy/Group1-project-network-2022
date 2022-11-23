4e0f8f17adf0067b5ae6971f41f0f97465a3f352
JeyKey09 <62012201+JeyKey09@users.noreply.github.com>
2022-11-23 16:23:28 +0100

    fixed some in the mainController

--- src/main/java/no/ntnu/idata2304/group1/clientapp/app2/ui/MainController.java
+++ src/main/java/no/ntnu/idata2304/group1/clientapp/app2/ui/MainController.java
@@ -9,95 +9,100 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
-import no.ntnu.idata2304.group1.clientapp.app2.ClientSocket;
+import no.ntnu.idata2304.group1.clientapp.app2.network.ClientSocket;
 import no.ntnu.idata2304.group1.data.Room;
 import no.ntnu.idata2304.group1.data.Sensor;
 
 /**
  * TODO: intergrate better with ClienSocket
- * ----------------------------------------------------------------------
- * Main controller for the UI.
- * Creates and loads the main window and loads the room windows into the main window.
+ * ---------------------------------------------------------------------- Main controller for the
+ * UI. Creates and loads the main window and loads the room windows into the main window.
  */
 public class MainController extends Application {
-  private MainWindowController mainWindowController;
-  private ArrayList<RoomWindowController> roomWindowControllers;
-  private FlowPane flowPane;
+    private MainWindowController mainWindowController;
+    private ArrayList<RoomWindowController> roomWindowControllers;
+    private FlowPane flowPane;
 
 
-  /**
-  * starts the application.
-  *
-  * @param stage the stage
-  *
-  * @throws IOException if the fxml file could not be loaded
-  */
-  public void start(Stage stage){
-    ClientSocket clientSocket;
-      this.roomWindowControllers = new ArrayList<>();
-      try{
-      clientSocket = new ClientSocket("10.24.90.163", 6008, "C:\\Users\\johan\\Desktop\\Group1testfolder\\src\\test\\resources\\no\\ntnu\\idata2304\\group1\\clientapp\\app2\\trustedCerts");
-      clientSocket.getListOfRooms();
-      for(int i = 0, i < clientSocket.getListOfRooms(); clientSocket.getListOfRooms();){
-        addRoom(clientSocket.getListOfRooms().get(i), stage);
-      }
-    } catch (IOException e) {
-          Alert alert = new Alert(AlertType.ERROR);
-          alert.setTitle("Error");
-          alert.setHeaderText("Could not connect to server");
-          alert.setContentText("Please check if the server is running");
-          alert.showAndWait();
-          System.exit(0);
-      }
+    /**
+     * starts the application.
+     *
+     * @param stage the stage
+     *
+     * @throws IOException if the fxml file could not be loaded
+     */
+    public void start(Stage stage) {
+        ClientSocket clientSocket;
+        this.roomWindowControllers = new ArrayList<>();
+        try {
+            clientSocket = new ClientSocket("10.24.90.163", 6008,
+                    "C:\\Users\\johan\\Desktop\\Group1testfolder\\src\\test\\resources\\no\\ntnu\\idata2304\\group1\\clientapp\\app2\\trustedCerts");
+            ArrayList<Room> rooms = clientSocket.getListOfRooms();
+            if (rooms != null) {
+                for (int i = 0; i < rooms.size(); i++) {
+                    addRoom(clientSocket.getListOfRooms().get(i), stage);
+                }
+            }
+        } catch (IOException e) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Could not connect to server");
+            alert.setContentText("Please check if the server is running");
+            alert.showAndWait();
+            System.exit(0);
+        }
 
-      //Loads mainScene
-      FXMLLoader mainWindowLoader = new FXMLLoader(MainController.class.getResource("MainScene.fxml"));
-      try{
+        // Loads mainScene
+        FXMLLoader mainWindowLoader =
+                new FXMLLoader(MainController.class.getResource("MainScene.fxml"));
+        try {
             Stage mainStage = makeStage(mainWindowLoader, stage, 1000, 700);
             mainStage.setTitle("Rooms");
         } catch (IOException e) {
             new Alert(AlertType.ERROR, "Could not load main window").showAndWait();
-      }
-      
-      mainWindowController = mainWindowLoader.getController();
+        }
 
-      //Initiates flowPane
-      this.flowPane = this.mainWindowController.flowPane;
+        mainWindowController = mainWindowLoader.getController();
 
-      //Binds the flowPane to the stage dimensions
-      this.flowPane.prefWidthProperty().bind(stage.widthProperty());
-      this.flowPane.prefHeightProperty().bind(stage.heightProperty());
-      //Example rooms.
-      try {
-        addExampleRooms(stage);
-    } catch (IOException e){
-        new Alert(Alert.AlertType.WARNING, e.getMessage());
-    }
+        // Initiates flowPane
+        this.flowPane = this.mainWindowController.flowPane;
+
+        // Binds the flowPane to the stage dimensions
+        this.flowPane.prefWidthProperty().bind(stage.widthProperty());
+        this.flowPane.prefHeightProperty().bind(stage.heightProperty());
+        // Example rooms.
+        try {
+            addExampleRooms(stage);
+        } catch (IOException e) {
+            new Alert(Alert.AlertType.WARNING, e.getMessage());
+        }
     }
 
 
     /**
      * Loads the rooms into the main window
+     * 
      * @param stage the main stage
      * @throws IOException if the fxml file could not be loaded;
      */
     public void addRoom(Room room, Stage stage) throws IOException {
-        FXMLLoader roomWindowLoader = new FXMLLoader(MainController.class.getResource("RoomScene.fxml"));
+        FXMLLoader roomWindowLoader =
+                new FXMLLoader(MainController.class.getResource("RoomScene.fxml"));
         flowPane.getChildren().add(roomWindowLoader.load());
         RoomWindowController roomWindowController = roomWindowLoader.getController();
         roomWindowControllers.add(roomWindowController);
         roomWindowController.setRoom(room);
-        //Sets the pane size and style
+        // Sets the pane size and style
 
 
 
-        //Sets the on click event for the room
+        // Sets the on click event for the room
         roomWindowController.getPane().setOnMouseClicked(mouseEvent -> {
             if (mouseEvent.getClickCount() == 2)
                 roomWindowController.expandRoomView(stage.getWidth(), stage.getHeight());
         });
 
-        //Contracts the roomWindow if the room is clicked
+        // Contracts the roomWindow if the room is clicked
         mainWindowController.getFlowPane().setOnMouseClicked(mouseEvent -> {
             if (mouseEvent.getClickCount() != 2) {
                 contractPane();
@@ -109,11 +114,13 @@ public class MainController extends Application {
      * Contracts the pane to the original size
      */
     private void contractPane() {
-        for (RoomWindowController roomWindowController : roomWindowControllers) roomWindowController.contractRoomView();
+        for (RoomWindowController roomWindowController : roomWindowControllers)
+            roomWindowController.contractRoomView();
     }
 
 
-    /** Creates a stage from a loader, stage, width and height.
+    /**
+     * Creates a stage from a loader, stage, width and height.
      *
      * @param fxmlLoader The loader to load the stage from.
      * @param stage The stage to load the loader into.
@@ -123,7 +130,8 @@ public class MainController extends Application {
      *
      * @throws IOException If the loader fails to load the stage.
      */
-    public Stage makeStage(FXMLLoader fxmlLoader, Stage stage, int width, int height) throws IOException {
+    public Stage makeStage(FXMLLoader fxmlLoader, Stage stage, int width, int height)
+            throws IOException {
         Scene scene = new Scene(fxmlLoader.load(), width, height);
         stage.setTitle("Hello!");
         stage.setScene(scene);
@@ -132,12 +140,11 @@ public class MainController extends Application {
     }
 
     /**
-     * TODO: Remove example methods
-     * Adds 10 example rooms to the roomWindowControllers
+     * TODO: Remove example methods Adds 10 example rooms to the roomWindowControllers
      */
     public void addExampleRooms(Stage stage) throws IOException {
         int n = 0;
-        while(n <= 2) {
+        while (n <= 2) {
             Room room = new Room(n, "Room From AddExampleRooms" + n);
             addRoom(room, stage);
             addExampleSensorsLive(n);
@@ -147,7 +154,7 @@ public class MainController extends Application {
 
     public void addExampleSensorsLive(int number) {
         ArrayList<Sensor> sensorList = new ArrayList<>();
-        for(int count = 0; count < 3; count++) {
+        for (int count = 0; count < 3; count++) {
             Sensor sensor = new Sensor(Sensor.Types.TEMPERATURE, number + "");
             sensorList.add(sensor);
         }
