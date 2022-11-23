ff5ed0c4226d0553a9c55babab9bb17822b5a60f
JeyKey09 <62012201+JeyKey09@users.noreply.github.com>
2022-11-23 14:08:12 +0100

    Moved clientsocket to network in App since that makes more sense

--- src/test/java/no/ntnu/idata2304/group1/clientapp/app2/network/ClientSocketTest.java
+++ src/test/java/no/ntnu/idata2304/group1/clientapp/app2/network/ClientSocketTest.java
@@ -1,4 +1,4 @@
-package no.ntnu.idata2304.group1.clientapp.app2;
+package no.ntnu.idata2304.group1.clientapp.app2.network;
 
 import no.ntnu.idata2304.group1.data.network.Message;
 import no.ntnu.idata2304.group1.data.network.responses.ErrorMessage;
