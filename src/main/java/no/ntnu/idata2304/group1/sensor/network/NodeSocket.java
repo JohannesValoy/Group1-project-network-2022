576a0f423dfcbcebfe2cf57e042eb4371ad66f90
JeyKey09 <62012201+JeyKey09@users.noreply.github.com>
2022-11-30 12:27:53 +0100

    Removed some imports

--- src/main/java/no/ntnu/idata2304/group1/sensor/network/NodeSocket.java
+++ src/main/java/no/ntnu/idata2304/group1/sensor/network/NodeSocket.java
@@ -3,21 +3,14 @@ package no.ntnu.idata2304.group1.sensor.network;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
+import java.util.logging.Logger;
 import javax.net.SocketFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import no.ntnu.idata2304.group1.clientapp.app2.network.SSLTrustFactory;
-import no.ntnu.idata2304.group1.data.Room;
-import no.ntnu.idata2304.group1.data.Sensor;
 import no.ntnu.idata2304.group1.data.network.Message;
 import no.ntnu.idata2304.group1.data.network.requests.add.AddMessage;
-import no.ntnu.idata2304.group1.data.network.requests.get.GetLogsMessage;
-import no.ntnu.idata2304.group1.data.network.responses.DataMessage;
 import no.ntnu.idata2304.group1.data.network.responses.ErrorMessage;
 import no.ntnu.idata2304.group1.data.network.responses.OKMessage;
 
@@ -29,12 +22,13 @@ public class NodeSocket {
     private SSLSocket socket;
     private ObjectInputStream input;
     private ObjectOutputStream output;
+    private static Logger logger = Logger.getLogger(NodeSocket.class.getName());
 
     /**
      * Instantiates a new Node socket.
      *
      * @param hostname the hostname
-     * @param port     the port
+     * @param port the port
      * @throws IOException the io exception
      */
     public NodeSocket(String hostname, int port) throws IOException {
@@ -48,8 +42,8 @@ public class NodeSocket {
     /**
      * Instantiates a new Node socket.
      *
-     * @param hostname            the hostname
-     * @param port                the port
+     * @param hostname the hostname
+     * @param port the port
      * @param folderForCustomCert the folder for custom cert
      * @throws IOException the io exception
      */
@@ -69,7 +63,7 @@ public class NodeSocket {
      * Response message.
      *
      * @return the message
-     * @throws IOException            the io exception
+     * @throws IOException the io exception
      * @throws ClassNotFoundException the class not found exception
      */
     public Message response() throws IOException, ClassNotFoundException {
@@ -86,43 +80,18 @@ public class NodeSocket {
         };
     }
 
-
-    /**
-     * Add room data array list.
-     *
-     * @param rooms the rooms
-     * @return the array list
-     * @throws IOException            the io exception
-     * @throws ClassNotFoundException the class not found exception
-     */
-    public ArrayList<Room> addRoomData(List<String> rooms)
-            throws IOException, ClassNotFoundException {
-        output.writeObject(new GetLogsMessage(Sensor.Types.TEMPERATURE, (ArrayList<String>) rooms));
-        Message messageResponse = response();
-        ArrayList<Room> data = new ArrayList<>();
-        if (messageResponse.getType() == Message.Types.OK) {
-            DataMessage response = (DataMessage) messageResponse;
-            for (Iterator<Object> it = response.getData(); it.hasNext();) {
-                Room room = (Room) it.next();
-                data.add(room);
-            }
-
-        }
-        return data;
-    }
-
     /**
      * Send data.
      *
      * @param lastTemperatureReading the last temperature reading
-     * @param apiKey                 the api key
+     * @param apiKey the api key
      */
     public void sendData(double lastTemperatureReading, String apiKey) {
         try {
             output.writeObject(
                     new AddMessage(AddMessage.Command.LOG, apiKey, lastTemperatureReading));
         } catch (Exception e) {
-
+            logger.severe("Could not send data to server. " + e.getMessage());
         }
     }
 
